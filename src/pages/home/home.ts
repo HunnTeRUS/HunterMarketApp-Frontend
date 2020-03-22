79c0e6f337e2551f6dfc7621f07d52956ed2b912
HunnTeRUS <otavio201378@gmail.com>
2020-03-21 21:57:51 -0300

    Navigation

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -12,39 +12,9 @@ export class HomePage {
 
   }
 
+  //Metodo para empilhar as paginas
+  login(){
+  this.navCtrl.setRoot('CategoriesPage');
 }
 
-/*export class LoginPage {
-  // The account fields for the login form.
-  // If you're using the username field with or without email, make
-  // sure to add it to the type
-  account = {
-    username: 'yajuve',
-    fullname: 'Mohamed Raouf',
-    email: 'yajuve.25.dz@gmail.com',
-    password: 'demodemo'
-  };
-
-  // Our translated text strings
-  private loginErrorString: string;
-  private opt: string = 'signin';
-
-  constructor(public http:HttpProvider, public userProvider: UserProvider, public menuCtrl: MenuController, public navCtrl: NavController,
-    public translateService: TranslateService) {
-    this.menuCtrl.enable(false);
-    this.translateService.get('LOGIN_ERROR').subscribe((value) => {
-      this.loginErrorString = value;
-    })
-  }
-
-  // Attempt to login in through our User service
-  doLogin() {
-    this.http.get('my-profile.json').subscribe((profile) => {
-      this.userProvider.user = <User>profile;
-      this.navCtrl.setRoot('ListFriendsPage');
-    }, (err) => {
-      console.error(err);
-    });
-
-  }
-}*/
+}
