afbdbc0c362628d812c881c0674e12e342f0314f
HunnTeRUS <otavio201378@gmail.com>
2020-03-23 22:46:27 -0300

    Dynamically added cities and states

--- src/pages/signup/signup.module.ts
+++ src/pages/signup/signup.module.ts
@@ -1,6 +1,8 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { SignupPage } from './signup';
+import { EstadoService } from '../../services/domain/estado.service';
+import { CidadeService } from '../../services/domain/cidade.service';
 
 @NgModule({
   declarations: [
@@ -9,5 +11,9 @@ import { SignupPage } from './signup';
   imports: [
     IonicPageModule.forChild(SignupPage),
   ],
+  providers:[
+    CidadeService,
+    EstadoService
+  ]
 })
 export class SignupPageModule {}
