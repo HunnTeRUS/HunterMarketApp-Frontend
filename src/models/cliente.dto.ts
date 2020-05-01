e2ee2da9e8af95cfd15576c5efd97642654851d3
HunnTeRUS <otavio201378@gmail.com>
2020-04-30 23:21:27 -0300

    Profile page

--- src/models/cliente.dto.ts
+++ src/models/cliente.dto.ts
@@ -1,6 +1,11 @@
+import { EnderecoDTO } from "./endereco.dto";
+
 export interface ClienteDTO {
     id : string;
     nome : string;
     email : string;
     imageUrl? : string;
+    cpfOuCnpj: string;
+    enderecos: EnderecoDTO[];
+    telefones: string[];
 }
