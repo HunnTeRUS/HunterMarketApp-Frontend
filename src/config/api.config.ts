7548caa8e59d2047891f37d4eb3ee89291550623
HunnTeRUS <otavio201378@gmail.com>
2020-05-21 23:01:00 -0300

    Updated pictures and deploy

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,4 +1,4 @@
 export const API_CONFIG = {
-    baseUrl: "http://localhost:8080",
+    baseUrl: "https://hunter-market-app.herokuapp.com",
     bucketBaseUrl: "https://hunterus.s3-sa-east-1.amazonaws.com"
 }
